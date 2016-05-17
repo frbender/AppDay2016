032c93deb644ec852aadca98a39aa1c0e91dc208
Franz Bender <franz.bender@uni-rostock.de>
2016-05-17 14:37:40 +0200

    Another Test

--- WebTest/main.py
+++ WebTest/main.py
@@ -1,5 +1,23 @@
-a = [1, 2, 3, 4, 5]
+from http.server import BaseHTTPRequestHandler, HTTPServer
 
+
+class WebRequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        # Send response status code
+        self.send_response(200)
+
+        # Send headers
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+        # Send message back to client
+        message = "Hello world!"
+        # Write content as utf-8 data
+        self.wfile.write(bytes(message, "utf8"))
+        return
+
+
+a = [1, 2, 3, 4, 5]
 
 def reverse(x):
     if len(x) == 0:
@@ -7,5 +25,15 @@ def reverse(x):
     a, b = (x[0], x[1:])
     return reverse(b) + [a]
 
-
 print(reverse(a))
+
+
+def run():
+    print("starting server...")
+    serverAddress = ("127.0.0.1", 8081)
+    httpd = HTTPServer(serverAddress, WebRequestHandler)
+    print("running server...")
+    httpd.serve_forever()
+
+
+run()

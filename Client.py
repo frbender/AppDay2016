4117ff8c06d15cb57dffaa4e4d69d3d68710cb59
adrianlut <ich@adrian-lutsch.de>
2016-05-19 00:56:35 +0200

    Debug messages added

--- Client.py
+++ Client.py
@@ -7,26 +7,33 @@ class Client():
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.s.setblocking(False)
         try:
-            self.isAlive = True
             self.s.connect(self.addr)
         except socket.error as e:
-            print("Error in Client.py: init   " + str(e))
+            if e.errno != 36:
+                print("[Client.__init__] ERROR connect failed! Msg:", str(e))
+            else:
+                self.isAlive = True
+                print("[Client.__init__] Connected")
 
     def send(self, ip = "", message = "") -> bool:
         try:
             self.s.sendall(bytes(message, encoding='utf-8'))
+            print("[Client.send] Message send")
             return True
         except OSError:
-            print("Error in Client.py: send")
+            print("[Client.send] ERROR failed! Msg:", str(e))
             return False
 
     def recv(self) -> str:
         try:
             message = self.s.recv(2048)
             if not message:
+                print("[Client.recv] Closing")
                 self.close()
+                return None
             else:
-                return message
+                print("[Client.recv] Message receved")
+                return message.decode()
         except socket.error as e:
             #print(str(e))
             return None
@@ -36,7 +43,11 @@ class Client():
 
     def close(self):
         self.isAlive = False
-        self.s.close()
+        try:
+            self.s.close()
+            print("[Client.close] Closed!")
+        except socket.error as e:
+            print("[Client.close] ERROR failed! Msg:", str(e))
 
 # c = Client(("127.0.0.1", 50000))
 #
--- Server.py
+++ Server.py
@@ -12,8 +12,11 @@ class Server(threading.Thread):
     def __init__(self, addr : (str,int)):
         threading.Thread.__init__(self)
         self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server.bind(addr)
-        self.server.listen(1)
+        try:
+            self.server.bind(addr)
+            self.server.listen(1)
+        except socket.error as e:
+            print("[Server.__init__] ERROR binding failed! Msg:", str(e))
         self._stopme = threading.Event()
 
     def run(self):
@@ -25,33 +28,39 @@ class Server(threading.Thread):
                     if sock is self.server:
                         client, addr = self.server.accept()
                         self.clients.append(client)
-                        print("Client {} verbunden".format(addr[0]))
+                        print("[Server.run.<add>] Client {} connected".format(addr[0]))
                     else:
                         nachricht = sock.recv(1024)
                         ip = sock.getpeername()[0]
                         if nachricht:
-                            print("[{}] {}".format(ip, nachricht.decode()))
+                            print("[Server.run.<recv>] {}: {}".format(ip, nachricht.decode()))
                         else:
-                            print("Verbindung zu {} beendet".format(ip))
                             sock.close()
                             Server.clients.remove(sock)
                             if sock in schreiben:
                                 schreiben.remove(sock)
+                            print("[Server.run.<recv>] Connection with {} closed".format(ip))
                 for sock in schreiben:
                     ip = sock.getpeername()[0]
                     if ip in Server.senddict:
-                        sock.sendall(bytes(Server.senddict.pop(ip), 'utf-8'))
+                        message = Server.senddict.pop(ip)
+                        sock.sendall(bytes(message, 'utf-8'))
+                        print("[Server.run.<send>] {} sent to {} ".format(message, ip))
                 if len(Server.senddict) > 0:
-                    print("Not all messages send!")
+                    print("[Server.run.<send>] WARN Not all messages send!")
                 Server.Lock.release()
         finally:
-            print("Closing all clients")
-            for c in Server.clients:
-                c.close()
-            self.server.close()
+            print("[Server.run.<finally>] Closing all clients")
+            try:
+                for c in Server.clients:
+                    c.close()
+                self.server.close()
+            except socket.error as e:
+                print("[Server.run.<finally>] ERROR on closing clients Msg:", str(e))
 
     def stop(self):
         self._stopme.set()
+        print("[Server.stop] stop flag set")
 
     def stopped(self):
         return self._stopme.isSet()
@@ -60,6 +69,7 @@ class Server(threading.Thread):
         Server.Lock.acquire()
         Server.senddict[ip] = message
         Server.Lock.release()
+        print("[Server.send] Message {} added to senddict".format(message))
 
 # s = Server(("",50000))
 # s.start()

c9dcf8a0ff8e2bbf375a4b07ee78dd61b9a8d9c1
adrianlut <ich@adrian-lutsch.de>
2016-05-18 15:21:01 +0200

    Erste Version server.py

---
+++ server.py
@@ -0,0 +1,33 @@
+import signal
+import socketserver
+import sys
+
+
+class ChatRequestHandler(socketserver.BaseRequestHandler):
+    def handle(self):
+        addr = self.client_address[0]
+        print("[{}] Verbindung hergestellt".format(addr))
+
+        while True:
+            s = self.request.recv(1024)
+            if s:
+                print("[{}] {}".format(addr, s.decode()))
+            else:
+                print("[{}] Verbindung geschlossen".format(addr))
+                break
+
+
+def signalHandler(signal, frame):
+    global server
+    print("Closing because of kill")
+    server.shutdown()
+    sys.exit(0)
+
+
+signal.signal(signal.SIGTERM, signalHandler)
+server = socketserver.ThreadingTCPServer(("", 50000), ChatRequestHandler)
+
+try:
+    server.serve_forever()
+except KeyboardInterrupt:
+    server.shutdown()
